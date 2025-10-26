5a429fca143ba4e95e2bae9ab2b4ad1c4f0e279a
Dmitry Chigrin <>
2025-10-26 17:11:39 +0500

    feat: add delete function code

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import { unlink, access, constants } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-  // Write your code here
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+  
+  try {
+    await access(filePath, constants.F_OK);
+    
+    await unlink(filePath);
+    console.log('File deleted successfully');
+    
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+    throw error;
+  }
 };
 
 await remove();
