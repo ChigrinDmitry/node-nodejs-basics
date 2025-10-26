3480e31c4e0cdea03cbdda4e0ddbde6cf0b28394
Dmitry Chigrin <>
2025-10-27 02:08:12 +0500

    feat: add read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  
+  const fileStream = createReadStream(filePath);
+  
+  fileStream.pipe(process.stdout);
+  
+  fileStream.on('error', (error) => {
+    console.error('Error reading file:', error.message);
+  });
 };
 
 await read();
