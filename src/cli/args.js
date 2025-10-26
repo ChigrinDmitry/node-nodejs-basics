c02eaf64cb223a84b5a3afcdcecd2a373f47303a
Dmitry Chigrin <>
2025-10-26 18:10:42 +0500

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+  
+  const parsedArgs = [];
+  
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace('--', ''); // Remove -- prefix
+    const value = args[i + 1];
+    
+    if (value !== undefined) {
+      parsedArgs.push(`${propName} is ${value}`);
+    }
+  }
+  
+  console.log(parsedArgs.join(', '));
 };
 
 parseArgs();
